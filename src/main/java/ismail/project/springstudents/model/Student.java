01c4dc474325770321c5d0af811713ff5421956d
Ismail <krystal98rus@gmail.com>
2024-02-27 16:52:11 +0100

    funcciona todo

--- src/main/java/ismail/project/springstudents/model/Student.java
+++ src/main/java/ismail/project/springstudents/model/Student.java
@@ -1,18 +1,25 @@
 package ismail.project.springstudents.model;
 
+import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.LocalDate;
+import java.time.Period;
 
 @Data
-@Builder
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String firstName;
     private String lastName;
     private LocalDate dateOfBirth;
+    @Column(unique = true)
     private String email;
+    @Transient
     private int age;
 
-
-
+    public int getAge(){
+        return Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
 }
